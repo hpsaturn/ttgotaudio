a7857ebb2e8ab1b7670b5a2c8bc11e8c3ca0aeb9
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-05 22:09:34 -0500

    added gyro fields and millis count

--- src/main.cpp
+++ src/main.cpp
@@ -8,36 +8,42 @@
 MPU9250 mpu;
 
 void setup() {
-    while(!Serial);
-    Serial.begin(115200);
-    Serial.println("started");
+  while(!Serial);
+  Serial.begin(115200);
+  Serial.println("started");
 
-#ifdef _ESP32_HAL_I2C_H_
-    // for esp32
-    Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
-#else
-    Wire.begin();
-#endif
+  #ifdef _ESP32_HAL_I2C_H_
+  // for esp32
+  Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
+  #else
+  Wire.begin();
+  #endif
 
-    mpu.setWire(&Wire);
-    mpu.beginAccel();
-    mpu.beginMag();
-    // you can set your own offset for mag values
-    // mpu.magXOffset = -50;
-    // mpu.magYOffset = -55;
-    // mpu.magZOffset = -10;
+  mpu.setWire(&Wire);
+  mpu.beginAccel();
+  mpu.beginMag();
+  mpu.beginGyro();
+  // you can set your own offset for mag values
+  // mpu.magXOffset = -50;
+  // mpu.magYOffset = -55;
+  // mpu.magZOffset = -10;
+}
+
+void mpuLoop(){
+  mpu.accelUpdate();
+  char buffer [60];
+  float mSqr = mpu.accelSqrt();
+  sprintf(buffer,"aX:%+.3f aY:%+.3f aZ:%+.3f aSqr:%.3f",mpu.accelX(),mpu.accelY(),mpu.accelX(),mSqr);
+  Serial.print(buffer);
+  mpu.magUpdate();
+  sprintf(buffer," mX:%+08.3f mY:%+08.3f mZ:%+08.3f mDir:%+08.3f",mpu.magX(),mpu.magY(),mpu.magZ(),mpu.magHorizDirection());
+  Serial.print(buffer);
+  mpu.gyroUpdate();
+  sprintf(buffer," gX:%+08.3f gY:%+08.3f gZ:%+08.3f ms:%lu",mpu.gyroX(),mpu.gyroY(),mpu.gyroZ(),millis());
+  Serial.println(buffer);
 }
 
 void loop() {
-    mpu.accelUpdate();
-    char buffer [40];
-    sprintf(buffer,"aX:%+06.3f aY:%+06.3f aZ:%+06.3f",mpu.accelX(),mpu.accelY(),mpu.accelX());
-    Serial.print(buffer);
-    mpu.magUpdate();
-    sprintf(buffer," mX:%+08.3f mY:%+08.3f mZ:%+08.3f",mpu.magX(),mpu.magY(),mpu.magZ());
-    Serial.println(buffer);
-    // Serial.println("accelSqrt: " + String(mpu.accelSqrt()));
-    // Serial.println("horizontal direction: " + String(mpu.magHorizDirection()));
-    // Serial.println("at " + String(millis()) + "ms");
-    delay(100);
+  mpuLoop();
+  delay(5);
 }
